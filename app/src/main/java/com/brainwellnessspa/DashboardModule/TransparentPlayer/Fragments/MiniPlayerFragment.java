4635328374349b87d0ad22cb4b1284e2b20e5914
admin <vishal@qltech.com.au>
2020-12-10 12:23:46 +0530

    Initial Commit Exoplayer

---
+++ app/src/main/java/com/brainwellnessspa/DashboardModule/TransparentPlayer/Fragments/MiniPlayerFragment.java
@@ -0,0 +1,26 @@
+package com.brainwellnessspa.DashboardModule.TransparentPlayer.Fragments;
+
+import android.os.Bundle;
+
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.brainwellnessspa.R;
+import com.brainwellnessspa.databinding.FragmentMiniPlayerBinding;
+
+public class MiniPlayerFragment extends Fragment {
+    FragmentMiniPlayerBinding binding;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_mini_player, container, false);
+        view = binding.getRoot();
+
+        return view;
+    }
+}
