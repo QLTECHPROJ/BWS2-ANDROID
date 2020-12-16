669964b72d1c918290c3b08193bea86f57680f32
admin <vishal@qltech.com.au>
2020-12-16 22:46:22 +0530

    Initial Commit Exoplayer

--- app/src/main/java/com/brainwellnessspa/DashboardModule/Playlist/PlaylistFragment.java
+++ app/src/main/java/com/brainwellnessspa/DashboardModule/Playlist/PlaylistFragment.java
@@ -177,22 +177,23 @@ public class PlaylistFragment extends Fragment {
                 editorr.clear();
                 editorr.commit();
                 callNewPlayerRelease();
-                SharedPreferences shared = getActivity().getSharedPreferences(CONSTANTS.PREF_KEY_AUDIO, MODE_PRIVATE);
-                AudioFlag = shared.getString(CONSTANTS.PREF_KEY_AudioFlag, "0");
-                if (!AudioFlag.equalsIgnoreCase("0")) {
-                    Fragment fragment = new MiniPlayerFragment();
-                    FragmentManager fragmentManager1 = getActivity().getSupportFragmentManager();
-                    fragmentManager1.beginTransaction()
-                            .add(R.id.flContainer, fragment)
-                            .commit();
-                    LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-                    params.setMargins(0, 6, 0, 200);
-                    binding.llSpace.setLayoutParams(params);
-                } else {
-                    LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-                    params.setMargins(0, 6, 0, 0);
-                    binding.llSpace.setLayoutParams(params);
-                }
+
+            }
+            SharedPreferences shared = getActivity().getSharedPreferences(CONSTANTS.PREF_KEY_AUDIO, MODE_PRIVATE);
+            AudioFlag = shared.getString(CONSTANTS.PREF_KEY_AudioFlag, "0");
+            if (!AudioFlag.equalsIgnoreCase("0")) {
+                Fragment fragment = new MiniPlayerFragment();
+                FragmentManager fragmentManager1 = getActivity().getSupportFragmentManager();
+                fragmentManager1.beginTransaction()
+                        .add(R.id.flContainer, fragment)
+                        .commit();
+                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                params.setMargins(0, 6, 0, 200);
+                binding.llSpace.setLayoutParams(params);
+            } else {
+                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                params.setMargins(0, 6, 0, 0);
+                binding.llSpace.setLayoutParams(params);
             }
         } catch (Exception e) {
             e.printStackTrace();
