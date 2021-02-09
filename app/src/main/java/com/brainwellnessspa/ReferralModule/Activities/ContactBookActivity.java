5a9a23d4e5e8d2f547f77164d9b1823024b94ccb
admin <vishal@qltech.com.au>
2021-02-09 16:38:54 +0530

    Initial commit

--- app/src/main/java/com/brainwellnessspa/ReferralModule/Activities/ContactBookActivity.java
+++ app/src/main/java/com/brainwellnessspa/ReferralModule/Activities/ContactBookActivity.java
@@ -281,10 +281,10 @@ public class ContactBookActivity extends AppCompatActivity {
             holder.binding.BtnInvite.setBackgroundResource(R.drawable.round_gray_cornor_normal);
             holder.binding.BtnInvite.setTextColor(getResources().getColor(R.color.gray));
             holder.binding.BtnInvite.setOnClickListener(v -> {
+                prepareContactData(model.getContactName(), model.getContactNumber());
                 notifyDataSetChanged();
                 holder.binding.BtnInvite.setBackgroundResource(R.drawable.round_blue_cornor_normal);
                 holder.binding.BtnInvite.setTextColor(getResources().getColor(R.color.white));
-                prepareContactData(model.getContactName(), model.getContactNumber());
             });
         }
 
