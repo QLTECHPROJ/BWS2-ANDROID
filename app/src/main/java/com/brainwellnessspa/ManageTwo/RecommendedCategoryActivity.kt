f83d7fbe5107ba050c0f9c121a9f2ebf529fded2
Foram <vishal@qltech.com.au>
2021-03-23 14:35:27 +0530

    bug

---
+++ app/src/main/java/com/brainwellnessspa/ManageTwo/RecommendedCategoryActivity.kt
@@ -0,0 +1,142 @@
+package com.brainwellnessspa.ManageTwo
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.brainwellnessspa.R
+import com.brainwellnessspa.UserModuleTwo.Models.UserListModel
+import com.brainwellnessspa.databinding.*
+
+
+class RecommendedCategoryActivity : AppCompatActivity() {
+    lateinit var binding: ActivityRecommendedCategoryBinding
+    lateinit var adapter: SelectedCategory
+    lateinit var ctx: Context
+    private lateinit var adapter1: AllCategory
+    private val userList = ArrayList<UserListModel>()
+    private val userList1 = ArrayList<UserListModel>()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_recommended_category)
+        ctx = this@RecommendedCategoryActivity
+        prepareUserData()
+        prepareHeaderData()
+        binding.rvSelectedCategory.layoutManager = GridLayoutManager(ctx, 3)
+        adapter = SelectedCategory(userList)
+        binding.rvSelectedCategory.adapter = adapter
+
+        binding.rvPerantCat.layoutManager = LinearLayoutManager(ctx)
+        adapter1 = AllCategory(userList1, ctx)
+        binding.rvPerantCat.adapter = adapter1
+    }
+    private fun prepareUserData() {
+        var userAdd = UserListModel("Parental Stress")
+        userList.add(userAdd)
+        userAdd = UserListModel("Parental Stress")
+        userList.add(userAdd)
+        userAdd = UserListModel("Parental Stress")
+        userList.add(userAdd)
+    }
+
+    private fun prepareHeaderData() {
+        var userAdd = UserListModel("Mental Health")
+        userList1.add(userAdd)
+        userAdd = UserListModel("Self - Development")
+        userList1.add(userAdd)
+        userAdd = UserListModel("Addiction")
+        userList1.add(userAdd)
+    }
+
+    class SelectedCategory(private val listModel: List<UserListModel>) : RecyclerView.Adapter<SelectedCategory.MyViewHolder>() {
+
+        inner class MyViewHolder(var bindingAdapter: SelectedCategoryRawBinding) : RecyclerView.ViewHolder(bindingAdapter.root)
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+            val v: SelectedCategoryRawBinding = DataBindingUtil.inflate(LayoutInflater.from(parent.context), R.layout.selected_category_raw, parent, false)
+            return MyViewHolder(v)
+        }
+
+        override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+            holder.bindingAdapter.tvCategory.text = listModel[position].name
+        }
+
+        override fun getItemCount(): Int {
+            return listModel.size
+        }
+    }
+
+    class AllCategory(private val listModel: List<UserListModel>, var ctx: Context) : RecyclerView.Adapter<AllCategory.MyViewHolder>() {
+        private lateinit var adapter2: ChildCategory
+        private val userList2 = ArrayList<UserListModel>()
+        private fun prepareCatData() {
+            var userAdd = UserListModel("Parental Stress")
+            userList2.add(userAdd)
+            userAdd = UserListModel("Motivation / Empowernment / Mindset")
+            userList2.add(userAdd)
+            userAdd = UserListModel("Communication / Self Epression / Public Speaking")
+            userList2.add(userAdd)
+            userAdd = UserListModel("Relationship Breakdown")
+            userList2.add(userAdd)
+            userAdd = UserListModel("Study and Eam Stress")
+            userList2.add(userAdd)
+            userAdd = UserListModel("Parental Stress")
+            userList2.add(userAdd)
+            userAdd = UserListModel("Motivation / Empowernment / Mindset")
+            userList2.add(userAdd)
+            userAdd = UserListModel("Communication / Self Epression / Public Speaking")
+            userList2.add(userAdd)
+            userAdd = UserListModel("Relationship Breakdown")
+            userList2.add(userAdd)
+            userAdd = UserListModel("Study and Eam Stress")
+            userList2.add(userAdd)
+            userAdd = UserListModel("Parental Stress")
+            userList2.add(userAdd)
+        }
+
+        inner class MyViewHolder(var bindingAdapter: AllCategoryRawBinding) : RecyclerView.ViewHolder(bindingAdapter.root)
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+            val v: AllCategoryRawBinding = DataBindingUtil.inflate(LayoutInflater.from(parent.context), R.layout.all_category_raw, parent, false)
+            return MyViewHolder(v)
+        }
+
+        override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+            holder.bindingAdapter.tvHeader.text = listModel[position].name
+
+            prepareCatData()
+            holder.bindingAdapter.rvChildCategory.layoutManager = StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL)
+            adapter2 = ChildCategory(userList2)
+            holder.bindingAdapter.rvChildCategory.adapter = adapter2
+        }
+
+        override fun getItemCount(): Int {
+            return listModel.size
+        }
+    }
+
+    class ChildCategory(private val listModel: List<UserListModel>) : RecyclerView.Adapter<ChildCategory.MyViewHolder>() {
+
+        inner class MyViewHolder(var bindingAdapter: AllCatDataRawBinding) : RecyclerView.ViewHolder(bindingAdapter.root)
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+            val v: AllCatDataRawBinding = DataBindingUtil.inflate(LayoutInflater.from(parent.context), R.layout.all_cat_data_raw, parent, false)
+            return MyViewHolder(v)
+        }
+
+        override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+            holder.bindingAdapter.tvText.text = listModel[position].name
+        }
+
+        override fun getItemCount(): Int {
+            return listModel.size
+        }
+    }
+
+}
---
+++ app/src/main/java/com/brainwellnessspa/ManageTwo/SleepTimeActivity.kt
@@ -0,0 +1,73 @@
+package com.brainwellnessspa.ManageTwo
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.brainwellnessspa.R
+import com.brainwellnessspa.UserModuleTwo.Models.UserListModel
+import com.brainwellnessspa.databinding.ActivitySleepTimeBinding
+import com.brainwellnessspa.databinding.SleepTimeRawBinding
+
+class SleepTimeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySleepTimeBinding
+    lateinit var adapter: SleepTimeAdapter
+    lateinit var ctx: Context
+    private val userList = ArrayList<UserListModel>()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_sleep_time)
+        ctx = this@SleepTimeActivity
+        prepareUserData()
+        binding.rvTimeSlot.layoutManager = GridLayoutManager(ctx,2)
+        adapter = SleepTimeAdapter(userList,ctx)
+        binding.rvTimeSlot.adapter = adapter
+    }
+
+    private fun prepareUserData() {
+        var userAdd = UserListModel("< 3")
+        userList.add(userAdd)
+        userAdd = UserListModel("3 - 4")
+        userList.add(userAdd)
+        userAdd = UserListModel("4 - 5")
+        userList.add(userAdd)
+        userAdd = UserListModel("5 - 6")
+        userList.add(userAdd)
+        userAdd = UserListModel("6 - 7")
+        userList.add(userAdd)
+        userAdd = UserListModel("7 - 8")
+        userList.add(userAdd)
+        userAdd = UserListModel("8 - 9")
+        userList.add(userAdd)
+        userAdd = UserListModel("9 - 10")
+        userList.add(userAdd)
+        userAdd = UserListModel("> 10")
+        userList.add(userAdd)
+    }
+    class SleepTimeAdapter(private val listModel: List<UserListModel>, var ctx: Context) : RecyclerView.Adapter<SleepTimeAdapter.MyViewHolder>() {
+
+        inner class MyViewHolder(var bindingAdapter: SleepTimeRawBinding) : RecyclerView.ViewHolder(bindingAdapter.root)
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+            val v: SleepTimeRawBinding = DataBindingUtil.inflate(LayoutInflater.from(parent.context), R.layout.sleep_time_raw, parent, false)
+            return MyViewHolder(v)
+        }
+
+        override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+            holder.bindingAdapter.tvhours.text = listModel[position].name
+            holder.bindingAdapter.llHourSlots.setOnClickListener {
+                val i = Intent(ctx,RecommendedCategoryActivity::class.java)
+                ctx.startActivity(i)
+            }
+        }
+
+        override fun getItemCount(): Int {
+            return listModel.size
+        }
+    }
+}
