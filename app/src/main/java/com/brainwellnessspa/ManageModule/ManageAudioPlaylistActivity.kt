bcf76de9069345512228b5e34c904417797be73f
Foram <vishal@qltech.com.au>
2021-03-25 15:28:48 +0530

    Screen 48,52 design only

---
+++ app/src/main/java/com/brainwellnessspa/ManageModule/ManageAudioPlaylistActivity.kt
@@ -0,0 +1,12 @@
+package com.brainwellnessspa.ManageModule
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.brainwellnessspa.R
+
+class ManageAudioPlaylistActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_manage_audio_playlist)
+    }
+}
