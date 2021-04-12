3206bf5c635aab1866531529a5e4cf8267040f01
Foram <vishal@qltech.com.au>
2021-04-12 12:51:22 +0530

    playlist detail model

--- app/src/main/java/com/brainwellnessspa/DashboardTwoModule/Model/PlaylistDetailsModel.kt
+++ app/src/main/java/com/brainwellnessspa/DashboardTwoModule/Model/PlaylistDetailsModel.kt
@@ -8,123 +8,122 @@ import com.google.gson.annotations.SerializedName
 class PlaylistDetailsModel {
     @SerializedName("ResponseData")
     @Expose
-    private val responseData: ResponseData? = null
+    val responseData: ResponseData? = null
 
     @SerializedName("ResponseCode")
     @Expose
-    private val responseCode: String? = null
+    val responseCode: String? = null
 
     @SerializedName("ResponseMessage")
     @Expose
-    private val responseMessage: String? = null
+    val responseMessage: String? = null
 
     @SerializedName("ResponseStatus")
     @Expose
-    private val responseStatus: String? = null
+    val responseStatus: String? = null
 
-
-}
-
-class ResponseData {
-    @SerializedName("PlaylistID")
-    @Expose
-    var playlistID: String? = null
-
-    @SerializedName("PlaylistName")
-    @Expose
-    var playlistName: String? = null
-
-    @SerializedName("PlaylistDesc")
-    @Expose
-    var playlistDesc: String? = null
-
-    @SerializedName("PlaylistMastercat")
-    @Expose
-    var playlistMastercat: String? = null
-
-    @SerializedName("PlaylistSubcat")
-    @Expose
-    var playlistSubcat: String? = null
-
-    @SerializedName("PlaylistImage")
-    @Expose
-    var playlistImage: String? = null
-
-    @SerializedName("PlaylistImageDetail")
-    @Expose
-    var playlistImageDetail: String? = null
-
-    @SerializedName("PlaylistSongs")
-    @Expose
-    var playlistSongs: List<PlaylistSong>? = null
-
-    @SerializedName("Created")
-    @Expose
-    var created: String? = null
-
-    @SerializedName("TotalAudio")
-    @Expose
-    var totalAudio: String? = null
-
-    @SerializedName("TotalDuration")
-    @Expose
-    var totalDuration: String? = null
-
-    @SerializedName("Totalhour")
-    @Expose
-    var totalhour: String? = null
-
-    @SerializedName("Totalminute")
-    @Expose
-    var totalminute: String? = null
-
-    class PlaylistSong {
-        @SerializedName("ID")
-        @Expose
-        var id: String? = null
-
-        @SerializedName("Name")
-        @Expose
-        var name: String? = null
-
-        @SerializedName("ImageFile")
-        @Expose
-        var imageFile: String? = null
-
+    class ResponseData {
         @SerializedName("PlaylistID")
         @Expose
         var playlistID: String? = null
 
-        @SerializedName("PSID")
+        @SerializedName("PlaylistName")
         @Expose
-        var psid: String? = null
+        var playlistName: String? = null
 
-        @SerializedName("SortId")
+        @SerializedName("PlaylistDesc")
         @Expose
-        var sortId: String? = null
+        var playlistDesc: String? = null
 
-        @SerializedName("AudioFile")
+        @SerializedName("PlaylistMastercat")
         @Expose
-        var audioFile: String? = null
+        var playlistMastercat: String? = null
 
-        @SerializedName("Audiomastercat")
+        @SerializedName("PlaylistSubcat")
         @Expose
-        var audiomastercat: String? = null
+        var playlistSubcat: String? = null
 
-        @SerializedName("AudioSubCategory")
+        @SerializedName("PlaylistImage")
         @Expose
-        var audioSubCategory: String? = null
+        var playlistImage: String? = null
 
-        @SerializedName("AudioDuration")
+        @SerializedName("PlaylistImageDetail")
         @Expose
-        var audioDuration: String? = null
+        var playlistImageDetail: String? = null
 
-        @SerializedName("AudioDirection")
+        @SerializedName("PlaylistSongs")
         @Expose
-        var audioDirection: String? = null
+        var playlistSongs: List<PlaylistSong>? = null
 
-        @SerializedName("Bitrate")
+        @SerializedName("Created")
         @Expose
-        var bitrate: String? = null
+        var created: String? = null
+
+        @SerializedName("TotalAudio")
+        @Expose
+        var totalAudio: String? = null
+
+        @SerializedName("TotalDuration")
+        @Expose
+        var totalDuration: String? = null
+
+        @SerializedName("Totalhour")
+        @Expose
+        var totalhour: String? = null
+
+        @SerializedName("Totalminute")
+        @Expose
+        var totalminute: String? = null
+
+        class PlaylistSong {
+            @SerializedName("ID")
+            @Expose
+            var id: String? = null
+
+            @SerializedName("Name")
+            @Expose
+            var name: String? = null
+
+            @SerializedName("ImageFile")
+            @Expose
+            var imageFile: String? = null
+
+            @SerializedName("PlaylistID")
+            @Expose
+            var playlistID: String? = null
+
+            @SerializedName("PSID")
+            @Expose
+            var psid: String? = null
+
+            @SerializedName("SortId")
+            @Expose
+            var sortId: String? = null
+
+            @SerializedName("AudioFile")
+            @Expose
+            var audioFile: String? = null
+
+            @SerializedName("Audiomastercat")
+            @Expose
+            var audiomastercat: String? = null
+
+            @SerializedName("AudioSubCategory")
+            @Expose
+            var audioSubCategory: String? = null
+
+            @SerializedName("AudioDuration")
+            @Expose
+            var audioDuration: String? = null
+
+            @SerializedName("AudioDirection")
+            @Expose
+            var audioDirection: String? = null
+
+            @SerializedName("Bitrate")
+            @Expose
+            var bitrate: String? = null
+        }
     }
+
 }
