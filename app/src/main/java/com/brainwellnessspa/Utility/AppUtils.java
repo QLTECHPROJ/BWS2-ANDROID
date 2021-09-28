ac908c4db4e2c7072614c57be6e708b4d8acfb83
Mansi <mansivaghela804@gmail.com>
2021-09-28 18:26:43 +0530

    - changes for live app

--- app/src/main/java/com/brainwellnessspa/Utility/AppUtils.java
+++ app/src/main/java/com/brainwellnessspa/Utility/AppUtils.java
@@ -6,12 +6,12 @@ public class AppUtils extends Application {
     public static final String MAIN_URL = "https://brainwellnessapp.com.au";
     public static final String STAGING_MAIN_URL = "http://brainwellnessapp.com.au/bwsapi/api/staging/v1/";
     public static final String Live_MAIN_URL = "http://brainwellnessapp.com.au/bwsapi/api/live/v1/";
-    public static final String BASE_URL = STAGING_MAIN_URL;
+    public static final String BASE_URL = Live_MAIN_URL;
 
     public static final String DEVELOPER_KEY = "AIzaSyD43ZM6bESb_pdSPzgcuCzKy8yD_45mlT8";
     public static final String YOUTUBE_VIDEO_CODE = "y1rfRW6WX08";
 
-    public static String tncs_url = MAIN_URL + "/terms-conditions/";
-    public static String privacy_policy_url = MAIN_URL + "/privacy-policy/";
-    public static String how_refer_works_url = MAIN_URL + "/how-refer-works/";
+    public static String tncs_url = Live_MAIN_URL + "/terms-conditions/";
+    public static String privacy_policy_url = Live_MAIN_URL + "/privacy-policy/";
+    public static String how_refer_works_url = Live_MAIN_URL + "/how-refer-works/";
 }
