196f64a668c18bfa58e8708fc31555512b379340
Foram <vishal@qltech.com.au>
2021-11-15 16:43:41 +0530

    url

--- app/src/main/java/com/brainwellnessspa/utility/AppUtils.kt
+++ app/src/main/java/com/brainwellnessspa/utility/AppUtils.kt
@@ -7,8 +7,8 @@ object AppUtils : Application() {
   /*  private const val STAGING_MAIN_URL = "https://brainwellnessapp.com.au/bwsapi/api/staging/v1/"
     const val Live_MAIN_URL = "https://brainwellnessapp.com.au/bwsapi/api/live/v1/"
     const val BASE_URL = STAGING_MAIN_URL*/
-    private const val STAGING_MAIN_URL = "https://brainwellnessapp.com.au/bwsapi/api/staging/v2/"
-    private const val LIVE_MAIN_URL = "https://brainwellnessapp.com.au/bwsapi/api/live/v2/"
+    private const val STAGING_MAIN_URL = "https://brainwellnessapp.com.au/bwsapi/api/staging/v2_1/"
+    private const val LIVE_MAIN_URL = "https://brainwellnessapp.com.au/bwsapi/api/live/v2_1/"
     const val New_BASE_URL = STAGING_MAIN_URL
     const val DEVELOPER_KEY = "AIzaSyD43ZM6bESb_pdSPzgcuCzKy8yD_45mlT8"
     const val YOUTUBE_VIDEO_CODE = "y1rfRW6WX08"
